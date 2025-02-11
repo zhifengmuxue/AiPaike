78a404dead36e460377532a34dea63b26e887582
zfmx <zhifengmuxue@163.com>
2025-02-11 20:39:14 +0800

    init: 初始化项目

---
+++ src/main/java/org/example/aipaike/AiPaikeApplication.java
@@ -0,0 +1,13 @@
+package org.example.aipaike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPaikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPaikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aipaike/AiPaikeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aipaike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPaikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
