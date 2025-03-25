44d0ac36ed910f7af2ae8a1af335ed9fc6096259
jyz <sx190522@163.com>
2025-03-25 22:46:58 +0800

    fix:排课与调课修复

--- src/main/java/top/zfmx/aipaike/util/GeneticAlgorithmUtils.java
+++ src/main/java/top/zfmx/aipaike/util/GeneticAlgorithmUtils.java
@@ -1,6 +1,5 @@
 package top.zfmx.aipaike.util;
 
-import jakarta.annotation.Resource;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,15 +8,9 @@ import top.zfmx.aipaike.entity.Classroom;
 import top.zfmx.aipaike.entity.Course;
 import top.zfmx.aipaike.entity.Schedule;
 import top.zfmx.aipaike.entity.ScheduleResult;
-import top.zfmx.aipaike.service.ClassroomService;
-import top.zfmx.aipaike.service.CourseService;
-import top.zfmx.aipaike.service.ScheduleResultService;
-import top.zfmx.aipaike.service.SchedulesService;
 
-import java.sql.*;
 import java.util.*;
 
-import static java.util.Arrays.stream;
 import static top.zfmx.aipaike.util.GeneticAlgorithmUtils.Algorithm.adjustConflictsToEvening;
 
 
@@ -305,7 +298,7 @@ public class GeneticAlgorithmUtils {
                 newPopulation.setIndividuals(individuals);
 
                 // 2. 生成新个体填充剩余位置
-                for (int i = newPopulation.getIndividuals().size(); i < popSize; i++) {
+                while (newPopulation.individuals.size() < popSize) {
                     // 轮盘赌选择父代
                     Individual parent1 = rouletteWheelSelection(population, fitnessValues);
                     Individual parent2 = rouletteWheelSelection(population, fitnessValues);
@@ -320,10 +313,10 @@ public class GeneticAlgorithmUtils {
 
                     // 变异操作
                     mutate(child);
-                    newPopulation.setIndividuals(Collections.singletonList(child));
+                    newPopulation.individuals.add(child);
                 }
 
-
+                population = newPopulation;
             }
         }
 
@@ -510,11 +503,10 @@ public class GeneticAlgorithmUtils {
                         if (hasConflict(gene1,gene2)){
                             adjustGeneToEvening(gene1,gene2);
 
-                            }
                         }
                     }
-
                 }
+            }
 
         }
 
